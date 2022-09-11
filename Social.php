ec65d607a4d2346570704d0abf9d0599a0f3ef4d
alycra <kobe@franssen.xyz>
2022-09-10 23:05:55 -0400

    integration

--- Social.php
+++ Social.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html>
-<?php
+<?
   require 'steamauth/steamauth.php';
 require 'steamauth/userInfo.php';
   if (isset($_SESSION['steamid']))
@@ -12,6 +11,8 @@ require 'steamauth/userInfo.php';
     #not loggedin
   }
 ?>
+
+<!DOCTYPE html>
 <html lang="en-US">
 
 <head>
@@ -19,32 +20,45 @@ require 'steamauth/userInfo.php';
     <title>Ryzen.me</title>
     <link rel="icon" href="https://ryzen.me/wp-content/uploads/2022/09/ogu_pfp-removebg-preview.png" />
 </head>
-
 <div class="navigation">
     <a class="active" href="main.html">Home</a>
     <a href="tickets.html">BuffShop</a>
 </div>
-
 <body>
+  <? if(isset($_SESSION['steamid'])) {?>
+    <img class="img-rounded" src="<?=$steamprofile['avatar'];?>"> <b><?=$steamprofile['personaname'];?></b><b class="caret"></b>
+        <button href="steamauth/logout.php"type="button" class="logout"> logout</button>
+    <a href="tickets.php">
+      <button type="button" class="shop">Shop</button>
+      </a>
+  <? } else{?>
+    <button type="button" class="login"> Login with Steam to access the shop</button>
+    <? echo loginbutton();?>
+<? } ?>
+
+
     <div class="wp-site-blocks">
         <figure class="wp-block-image size-full is-resized is-style-default" style="border-radius:0px"><img
                 loading="lazy" src="https://ryzen.me/wp-content/uploads/2022/09/ryzen-calligraphy.png" alt=""
                 class="wp-image-25" width="302" height="131"
                 srcset="https://ryzen.me/wp-content/uploads/2022/09/ryzen-calligraphy.png 302w, https://ryzen.me/wp-content/uploads/2022/09/ryzen-calligraphy-300x130.png 300w"
                 sizes="(max-width: 302px) 100vw, 302px"></figure>
-        <hr class="line"> <br>
+<hr class="line"> <br>
         <h2 style="color:#b651e0">Discord: <button onclick="disccopy()" title="Copy" class="minibutton">ryzen#8829
             </button>
             <br>CashApp: <button onclick="cashcopy()" title="Copy" class="minibutton">$Zaiga
             </button> <br>Telegram:
             <button onclick="telecopy()" title="Copy" class="minibutton">@highballer </button> <br> Steam:
-            <button onclick="steamtradecopy()" title="Open in new tab" class="minibutton">Steam trade link
-            </button>
-            <br>
+            <button onclick="steamtradecopy()" title="Open in new tab" class="minibutton">Steam trade link </button>
         </h2>
         <hr class="line">
-        <address class="footer">Author: Ryzen | Collaborator: Ezi</address>
     </div>
+    <address>
+        <div class="footer">
+            <p>Author:Ryzen</p>
+            <p>Collaborator: Ezi</p>
+            </footer>
+    </address>
 </body>
 <script>
     function paypcopy() {
@@ -67,4 +81,4 @@ require 'steamauth/userInfo.php';
     }
 </script>
 
-</htm
+</html>
