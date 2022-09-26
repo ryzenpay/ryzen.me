4b8ad9eaf1faabe4fabb486a6b710af3d5883d22
alycra <kobe@franssen.xyz>
2022-09-26 18:49:13 -0400

    using floatval as finish

--- inventory.php
+++ inventory.php
@@ -77,8 +77,7 @@ foreach ($inventory->rgDescriptions as $value => $v) {
                     while($row = $result->fetch_assoc() ){
             echo '<img src = "http://steamcommunity-a.akamaihd.net/economy/image/'.$icon_url.'" class="icon" alt="'.$name.'">';
 	                    echo '<label>'.$row["name"].' | $'.$row["price"]."</label><br>";
-                        $value += ($row["price"]);
-            var_dump(floatval($row["price"]));
+                        $value += (floatval($row["price"]));
             echo
                 '<script type="text/javascript">
             document.getElementById("value").setAttribute("value","'.$value.'")
