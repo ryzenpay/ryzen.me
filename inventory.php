ee3152f5180dadcada19b039612adbea4fa2f81c
alycra <kobe@franssen.xyz>
2022-09-26 18:06:45 -0400

    changing the rounding

--- inventory.php
+++ inventory.php
@@ -77,7 +77,7 @@ foreach ($inventory->rgDescriptions as $value => $v) {
                     while($row = $result->fetch_assoc() ){
             echo '<img src = "http://steamcommunity-a.akamaihd.net/economy/image/'.$icon_url.'" class="icon" alt="'.$name.'">';
 	                    echo '<label>'.$row["name"].' | $'.$row["price"]."</label><br>";
-                        $value += $row["price"];
+                        $value += floor($row["price"]);
             echo
                 '<script type="text/javascript">
             document.getElementById("value").setAttribute("value","'.number_format($value).'")
