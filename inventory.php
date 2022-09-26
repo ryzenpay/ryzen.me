010b05720b1ff7b7648da1fb75eae6b9c1e90a47
alycra <kobe@franssen.xyz>
2022-09-26 18:40:44 -0400

    test

--- inventory.php
+++ inventory.php
@@ -77,7 +77,8 @@ foreach ($inventory->rgDescriptions as $value => $v) {
                     while($row = $result->fetch_assoc() ){
             echo '<img src = "http://steamcommunity-a.akamaihd.net/economy/image/'.$icon_url.'" class="icon" alt="'.$name.'">';
 	                    echo '<label>'.$row["name"].' | $'.$row["price"]."</label><br>";
-                        $value += (number_format(intval($row["price"])));
+                        $value += (number_format((float)$row["price"]));
+            var_dump($value);
             echo
                 '<script type="text/javascript">
             document.getElementById("value").setAttribute("value","'.$value.'")
