cbf23493bb677dce1f7cf0dccddd2dbc6fe70f68
alycra <kobe@franssen.xyz>
2022-10-02 19:50:25 -0400

    css for the table

--- search.php
+++ search.php
@@ -79,7 +79,7 @@ for ($int = 1; $int < count($inputarray); $int++){
 
 $result = $conn->query($sql);
 if ($result->num_rows > 0){
-    echo '<table>';
+    echo '<table class="table">';
     echo '<tr><th>Name</th><th>price</th></tr>';
 while($row = $result->fetch_assoc() ){
     $name = $row["name"];
