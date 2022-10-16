0f81cac9d594082ac2dae55f675c04e175ff91a7
alycra <kobe@franssen.xyz>
2022-10-15 23:26:27 -0400

    forcing page to load before displaying

--- index.php
+++ index.php
@@ -1,47 +1,53 @@
 <!DOCTYPE html>
 <html lang="en-US">
+<div id="cover">
 
-<head>
-    <link rel="stylesheet" href="styles.css">
-    <title>Ryzen.me</title>
-    <link rel="icon" href="images/flavicon.png" />
-    <a href="index.php"><button type="button" class="headbutton" style="background-color: purple">Home</button></a>
-    <a href="inventory.php"><button type="button" class="headbutton">Inventory price
-            check</button></a>
-    <a href="search.php"><button type="button" class="headbutton">Buff database search</button></a>
-    <a href="exchange.php"><button type="button" class="headbutton">Exchange</button></a>
-</head>
-<hr class="headline" style="top: -10px;">
-<hr class="headline" style="bottom: -10px;">
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <title>Ryzen.me</title>
+        <link rel="icon" href="images/flavicon.png" />
+        <a href="index.php"><button type="button" class="headbutton" style="background-color: purple">Home</button></a>
+        <a href="inventory.php"><button type="button" class="headbutton">Inventory price
+                check</button></a>
+        <a href="search.php"><button type="button" class="headbutton">Buff database search</button></a>
+        <a href="exchange.php"><button type="button" class="headbutton">Exchange</button></a>
+    </head>
+    <hr class="headline" style="top: -10px;">
+    <hr class="headline" style="bottom: -10px;">
 
-<body>
-    <br><br>
-    <img src="images/ryzen_calligraphy.png" class="cover">
-    <hr class="line">
-    <h2 style="color:#b651e0">Discord: <button onclick="disccopy()" title="Copy" class="minibutton">ryzen#8829
-        </button>
-        <br>CashApp: <button onclick="cashcopy()" title="Copy" class="minibutton">$Zaiga
-        </button> <br>Telegram:
-        <button onclick="telecopy()" title="Open in new tab" class="minibutton">@highballer </button> <br> Steam:
-        <button onclick="steamtradecopy()" title="Open in new tab" class="minibutton">Steam trade link </button>
-        <br> Exchange server: <button onclick="discord()" title="Open in new tab" class="minibutton">Discord
-            (.gg/ryzen)</button>
-    </h2>
-    <hr class="line">
-    <address>
-        <div class="footer">
-            <p>Author:Ryzen</p>
-            <p>Collaborator: Ezi</p>
-            </footer>
-    </address>
-    <a href="TOS.php">
-        <button title="TOS" class="tos">TOS</button>
-    </a>
-    <a href="privpos.php">
-        <button title="privpos" class="privpos">Privacy Policy</button>
-    </a>
-</body>
+    <body>
+        <br><br>
+        <img src="images/ryzen_calligraphy.png" class="cover">
+        <hr class="line">
+        <h2 style="color:#b651e0">Discord: <button onclick="disccopy()" title="Copy" class="minibutton">ryzen#8829
+            </button>
+            <br>CashApp: <button onclick="cashcopy()" title="Copy" class="minibutton">$Zaiga
+            </button> <br>Telegram:
+            <button onclick="telecopy()" title="Open in new tab" class="minibutton">@highballer </button> <br> Steam:
+            <button onclick="steamtradecopy()" title="Open in new tab" class="minibutton">Steam trade link </button>
+            <br> Exchange server: <button onclick="discord()" title="Open in new tab" class="minibutton">Discord
+                (.gg/ryzen)</button>
+        </h2>
+        <hr class="line">
+        <address>
+            <div class="footer">
+                <p>Author:Ryzen</p>
+                <p>Collaborator: Ezi</p>
+                </footer>
+        </address>
+        <a href="TOS.php">
+            <button title="TOS" class="tos">TOS</button>
+        </a>
+        <a href="privpos.php">
+            <button title="privpos" class="privpos">Privacy Policy</button>
+        </a>
+    </body>
+</div>
 <script>
+$(window).on('load', function() {
+    $("#cover").hide();
+});
+
 function paypcopy() {
     navigator.clipboard.writeText("Nissangt420@gmail.com");
 }
