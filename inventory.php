0f2616b30a5ed72341977391c4b37b59a6539418
alycra <111833605+alycra@users.noreply.github.com>
2022-11-15 10:09:26 -0500

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -79,6 +79,7 @@ body {
             }
             $url = "https://steamcommunity.com/profiles/$id/inventory/json/730/2";
             $jsondata = curl_get_contents($url);
+            var_dump($jsondata);
             //$jsondata = file_get_contents($url, false, $browser);
             if($jsondata || $error !=null) {
                 $inventory = json_decode($jsondata);
