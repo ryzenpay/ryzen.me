b8a9e7a894a55f200b7eeebf39aa9d4627585849
alycra <111833605+alycra@users.noreply.github.com>
2022-11-16 14:05:31 -0500

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -174,6 +174,7 @@ function curl_get_contents($url)
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
   curl_setopt($ch,CURLOPT_ENCODING, '');
   $data = curl_exec($ch);
   curl_close($ch);
