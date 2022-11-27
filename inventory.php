214b1660914140fcda74c155c596713f164de901
Alycra <kobe@franssen.xyz>
2022-11-26 21:31:54 -0500

    givin it another shot

--- inventory.php
+++ inventory.php
@@ -102,8 +102,8 @@ body {
                         $name = $v->market_hash_name;
                         $icon_url = $v->icon_url;
                         if ($v->tradable == 0){
-                            if (isset($v->owner_descriptions[1])){
-                                $hold = $v->owner_descriptions->value;
+                            if (isset($v->owner_descriptions)){
+                                $hold = $v->owner_descriptions[1]->value;
                             }
                             else{
                                 $hold = "Not Tradeable";
